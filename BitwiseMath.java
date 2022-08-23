59f231fd6ec49336d4cb625795ffe3464b46e6ae
BeanFactory <98367091+aadiujoshi@users.noreply.github.com>
2022-08-22 23:30:05 -0500

    Update BitwiseMath.java

--- BitwiseMath.java
+++ BitwiseMath.java
@@ -1,25 +1,25 @@
 public class BitwiseMath
 {
     public static void main(String args[]){
-        System.out.println("answer   " + add(8, 2));
+        System.out.println("answer   " + add(111111, 1111));
     }
 
     public static int add(int a, int b){
-        byte carry = 0;
+        int carry = 0;
         int f = 0;
         for(int mask = 1; mask <= ((a > b) ? a : b); mask <<= 1){
-            byte a1 = (byte)((mask & a));
-            byte a2 = (byte)((mask & b));
+            int a1 = (int)((mask & a));
+            int a2 = (int)((mask & b));
 
             //reduce to 1 and 0
             while(a1 > 1) a1 >>= 1;
             while(a2 > 1) a2 >>= 1;
             
             //add binary no carry
-            byte n = (byte)((a1 | a2) << (a1 & a2));
+            int n = (int)((a1 | a2) << (a1 & a2));
 
             //carry
-            n = (byte)((carry << (~(a1 & a2)) & 1) | n);
+            n = (int)((carry << (~(a1 & a2)) & 1) | n);
 
             //match place value
             while(n < (mask << (a1 & a2)) && n != 0){ n <<= 1; }
@@ -28,7 +28,7 @@ public class BitwiseMath
             f |= n; 
 
             //calculate next carry
-            carry = (byte)(a1 & a2);
+            carry = (int)(a1 & a2);
         }
 
         return f;
